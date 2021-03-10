eb053a88ac5e116c0ddea6a22f3605909302ea42
Evere23 <Evere_23@hotmail.com>
2021-03-10 16:14:31 -0600

    Agregando css y js a los slide

---
+++ js/rangeslider.js
@@ -0,0 +1,17 @@
+var slider = document.getElementById("dinero");
+var output = document.getElementById("resdinero");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
+
+var slider2 = document.getElementById("dias");
+var output2 = document.getElementById("resdias");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
