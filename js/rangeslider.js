8ffd4d7fb2a9e7a0c7a5f4aadaf5b2a487d50157
Evere23 <Evere_23@hotmail.com>
2021-03-11 15:19:35 -0600

    Modificaciones  alos valores maximos de dias y precios

--- js/rangeslider.js
+++ js/rangeslider.js
@@ -1,3 +1,4 @@
+const interes = 8.4; 
 var slider = document.getElementById("dinero");
 var output = document.getElementById("resdinero");
 output.innerHTML = slider.value; // Display the default slider value
@@ -9,9 +10,14 @@ slider.oninput = function() {
 
 var slider2 = document.getElementById("dias");
 var output2 = document.getElementById("resdias");
-output.innerHTML = slider2.value; // Display the default slider value
+output2.innerHTML = slider2.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider2.oninput = function() {
   output2.innerHTML = slider2.value;
 } 
+
+var output3 = document.getElementById("pago");
+
+// Update the current Pago value (each time you drag the slider handle)
+output3.innerHTML = (slider2.value/360) * interes * slider.value;
