fac30db0e0da35007f364548e61b92cd8cd4e4b9
Evere23 <Evere_23@hotmail.com>
2021-03-11 16:40:19 -0600

    modificando formula de la mensaualidad

--- js/rangeslider.js
+++ js/rangeslider.js
@@ -1,8 +1,8 @@
 const interes = 8.4 / 100; 
 var slider = document.getElementById("dinero");
 var output = document.getElementById("resdinero");
-var slider2 = document.getElementById("dias");
-var output2 = document.getElementById("resdias");
+var slider2 = document.getElementById("meses");
+var output2 = document.getElementById("resmeses");
 var output3 = document.getElementById("pago");
 
 output.innerHTML = slider.value; // Display the default slider value
@@ -10,7 +10,9 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = slider.value;
-  var pago = (slider2.value/360) * interes * slider.value;
+  var pagomensual = slider.value/slider2.value;
+  var interesmensual = (slider2.value/12) * interes * slider.value;
+  var pago = pagomensual + interesmensual;
   output3.innerHTML = pago.toFixed(2);
 } 
 
@@ -19,6 +21,8 @@ output2.innerHTML = slider2.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider2.oninput = function() {
   output2.innerHTML = slider2.value;
-  var pago = (slider2.value/360) * interes * slider.value;
+  var pagomensual = slider.value/slider2.value;
+  var interesmensual = (slider2.value/12) * interes * slider.value;
+  var pago = pagomensual + interesmensual;
   output3.innerHTML = pago.toFixed(2);
 } 
