475c0c431fccf6b0cdbeaa4a8f92cbd81191b91f
Evere23 <Evere_23@hotmail.com>
2021-03-11 16:19:55 -0600

    Alineando textos de contacto

--- js/rangeslider.js
+++ js/rangeslider.js
@@ -1,30 +1,24 @@
-const interes = 8.4; 
+const interes = 8.4 / 100; 
 var slider = document.getElementById("dinero");
 var output = document.getElementById("resdinero");
+var slider2 = document.getElementById("dias");
+var output2 = document.getElementById("resdias");
+var output3 = document.getElementById("pago");
+
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = slider.value;
+  var pago = (slider2.value/360) * interes * slider.value;
+  output3.innerHTML = pago.toFixed(2);
 } 
 
-var slider2 = document.getElementById("dias");
-var output2 = document.getElementById("resdias");
 output2.innerHTML = slider2.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider2.oninput = function() {
   output2.innerHTML = slider2.value;
-} 
-
-var output3 = document.getElementById("pago");
-
-// Update the current Pago value (each time you drag the slider handle)
-slider.oninput = function() {
-	var pago = (slider2.value/360) * interes * slider.value;
-	output3.innerHTML = pago.toFixed(2);
-} 
-slider2.oninput = function() {
-	var pago = (slider2.value/360) * interes * slider.value;
-	output3.innerHTML = pago.toFixed(2);
+  var pago = (slider2.value/360) * interes * slider.value;
+  output3.innerHTML = pago.toFixed(2);
 } 
